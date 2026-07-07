790064477519c67a8fcbdbb98a5204d659fb5a8d
karenmahr <kmahr@byupathway.edu>
2026-07-06 22:02:18 -0300

    project01

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+const dns = require('node:dns');
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`)
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(PORT, () => { console.log(`Database is listening and node running on port ${PORT}`) });
+    }
 });
-
